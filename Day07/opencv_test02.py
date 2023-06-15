e61362127cf856c0ee0cb73cc042a92522bcec71
수민 <123913969+LaniJeong@users.noreply.github.com>
2023-06-15 15:32:11 +0900

    6월 15일_1차 저장

---
+++ Day07/opencv_test02.py
@@ -0,0 +1,32 @@
+import cv2
+
+# 01. 일반 이미지
+# img = cv2.imread('./images/bongsuk.jpg')
+
+# 02. 흑백 이미지
+# img = cv2.imread('./images/bongsuk.jpg',cv2.IMREAD_GRAYSCALE)
+
+# 04. 원본 두고 흑백 추가
+img = cv2.imread('./images/bongsuk.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# 03. 이미지 축소
+# img_small = cv2.resize(img, (200, 90))
+
+# 05. 이미지 자르기
+height, width, channel = img.shape
+print(height, width, channel)
+
+img_crop = img[:, :int(width / 2)]  # height, width
+gray_crop = gray[:, :int(width / 2)]
+
+# 06. 이미지 블러
+img_blur = cv2.blur(img_crop, (10,10))      # 숫자가 클수록 블러처리 증가
+
+# cv2.imshow('Original', img)
+# cv2.imshow('Gray', gray)
+cv2.imshow('Blur half', img_blur)
+cv2.imshow('Gray half', gray_crop)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
